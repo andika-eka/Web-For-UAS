15d7827c8046b20d3f72e9e4999d0d8c2f061aa7
andika-eka <putuandika387@gmail.com>
2021-06-21 15:05:53 +0800

    ui for  payment schedule

--- resources/js/components/dashboard.vue
+++ resources/js/components/dashboard.vue
@@ -6,6 +6,117 @@
             <hr>
             <div class="row">
                 <div class="col-sm " style="padding:30px; ">
+                    <div class="table-responsive shadow-lg border rounded list-group-item-danger" style=" padding-top: 30px;
+                                padding-right: 30px;
+                                padding-bottom: 30px;
+                                padding-left: 30px;
+                                font-size: 20px;">
+                                <h3 >pembayaran terlambat:</h3>
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>id</th>
+                                    <th>nama</th>
+                                    <th>no unit</th>
+                                    <th>harga</th>
+                                    <th>-</th>
+                                </tr>
+                            </thead>
+                            <tfoot class="thead-light">
+                                <tr>
+                                    <th>id</th>
+                                    <th>nama</th>
+                                    <th>no unit</th>
+                                    <th>harga</th>
+                                    <th>-</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                <tr v-for="(item, id) in res.null.data" :key="id" class="table-light">
+                                    <td>{{ item.S_id }}</td>
+                                    <!-- <td>{{ item.name }}</td> -->
+                                    <td>{{ item.nama }}
+                                    </td>
+                                    <td>{{ item.no_unit }}</td>
+                                    <td>Rp.{{ item.harga }}</td>
+                                    <td>
+                                        <router-link :to="{name: 'SewaDetails', params: { id: item.S_id }}"
+                                            class="badge  bg-primary">details</router-link>
+                                        <router-link :to="{name: 'PembayaranCreate', params: { id: item.S_id }}"type="button" class="badge badge-success" > buat
+                                pembayaran</router-link>
+                                    </td>
+                                </tr>
+                                <tr v-for="(item, id) in res.late.data" :key="id" class="table-light">
+                                    <td>{{ item.S_id }}</td>
+                                    <!-- <td>{{ item.name }}</td> -->
+                                    <td>{{ item.nama }}
+                                    </td>
+                                    <td>{{ item.no_unit }}</td>
+                                    <td>Rp.{{ item.harga }}</td>
+                                    <td>
+                                        <router-link :to="{name: 'SewaDetails', params: { id: item.S_id }}"
+                                            class="badge  bg-primary">details</router-link>
+                                        <router-link :to="{name: 'PembayaranCreate', params: { id: item.S_id }}"type="button" class="badge badge-success" > buat
+                                pembayaran</router-link>
+                                    </td>
+                                </tr>
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="table-responsive shadow-lg border rounded list-group-item-success" style=" padding-top: 30px;
+                                padding-right: 30px;
+                                padding-bottom: 30px;
+                                padding-left: 30px;
+                                font-size: 20px;">
+                                <h3 >pembayaran mendatang:</h3>
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>id</th>
+                                    <th>nama</th>
+                                    <th>no unit</th>
+                                    <th>harga</th>
+                                    <th>batas pembayaran</th>
+                                    <th>-</th>
+                                </tr>
+                            </thead>
+                            <tfoot class="thead-light">
+                                <tr>
+                                    <th>id</th>
+                                    <th>nama</th>
+                                    <th>no unit</th>
+                                    <th>harga</th>
+                                    <th>batas pembayaran</th>
+                                    <th>-</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                <tr v-for="(item, id) in res.due.data" :key="id" class="table-light">
+                                    <td>{{ item.S_id }}</td>
+                                    <!-- <td>{{ item.name }}</td> -->
+                                    <td>{{ item.nama }}
+                                    </td>
+                                    <td>{{ item.no_unit }}</td>
+                                    <td>Rp.{{ item.harga }}</td>
+                                    <td>Rp.{{ item.lastpaid }}</td>
+                                    <td>
+                                        <router-link :to="{name: 'SewaDetails', params: { id: item.S_id }}"
+                                            class="badge  bg-primary">details</router-link>
+                                            <router-link :to="{name: 'PembayaranCreate', params: { id: item.S_id }}"type="button" class="badge badge-success" > buat
+                                pembayaran</router-link>
+                                    </td>
+                                </tr>
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-sm" style="color:white; padding:30px">
                     <div class="card border-primary mb-3 shadow" style=" height :200px; font-size: 20px;">
                         <div class="card-header">jumlah penyewa</div>
                         <div class="card-body text-primary text-center">
@@ -35,8 +146,6 @@
                                 pembayaran</router-link> -->
                         </div>
                     </div>
-                </div>
-                <div class="col-sm" style="color:white; padding:30px">
                     <div class="card bg-success shadow " style=" height :200px; font-size: 20px;">
                         <div class="card-header">pendapatan</div>
                         <div class="card-body text-center">
